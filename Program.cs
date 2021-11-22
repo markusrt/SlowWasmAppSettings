fbaa24997ed4085b9ef518e6251675ecd7bac30a
Markus Reinhardt <tuxxilla@gmail.com>
2021-11-22 15:22:42 +0100

    Update to dotnet 6

--- Program.cs
+++ Program.cs
@@ -1,33 +1,20 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using SlowWasmAppSettings;
 
-namespace SlowWasmAppSettings
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+ // My code start
+builder.Services.AddSingleton<SlowWasmAppSettings.MyCode.MicStepsService>();
+builder.Services.Configure<SlowWasmAppSettings.MyCode.BreakpointSettings>(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    builder.Configuration.Bind(options);
+});
+// my code end
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            // My code start
-            builder.Services.AddSingleton<MyCode.MicStepsService>();
-            builder.Services.Configure<MyCode.BreakpointSettings>(options =>
-            {
-                builder.Configuration.Bind(options);
-            });
-            // my code end
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
